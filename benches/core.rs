3cea0d11257dc0e1c8fd2d6d042b5adb5e1be79b
Ben Webber <benjamin.webber@gmail.com>
2026-06-10 02:10:38 +0000

    Add range sum benchmark

--- benches/core.rs
+++ benches/core.rs
@@ -1,16 +1,13 @@
-use std::hint::black_box;
-
 use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
 use ferth::{Fe, io::NoIo};
-
-const FIB: &[u8] = b": fib dup 2 < if exit then dup 1- recurse swap 1- 1- recurse + ;";
+use std::hint::black_box;
 
 fn bench_fib(c: &mut Criterion) {
     c.bench_function("fib(20)", |b| {
         b.iter_batched(
             || {
                 let mut fe = Fe::new([0u8; 65536], NoIo).unwrap();
-                fe.evaluate(FIB).unwrap();
+                fe.evaluate(include_bytes!("core.fth")).unwrap();
                 fe
             },
             |mut fe| fe.evaluate(black_box(b"20 fib drop")).unwrap(),
@@ -19,5 +16,19 @@ fn bench_fib(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_fib);
+fn bench_range_sum(c: &mut Criterion) {
+    c.bench_function("range_sum(1_000_000)", |b| {
+        b.iter_batched(
+            || {
+                let mut fe = Fe::new([0u8; 65536], NoIo).unwrap();
+                fe.evaluate(include_bytes!("core.fth")).unwrap();
+                fe
+            },
+            |mut fe| fe.evaluate(black_box(b"1000000 range-sum drop")).unwrap(),
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(benches, bench_fib, bench_range_sum);
 criterion_main!(benches);
