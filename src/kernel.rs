0c69fdd2f32e3042064605483e5f7a3d2e7ecacd
Ben Webber <benjamin.webber@gmail.com>
2026-06-17 22:29:20 +0000

    Extract Environment from kernel module

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,6 +1,6 @@
 //! The outer interpreter.
 use crate::data::{Data, Mem};
-use crate::double::{Double, SignedDouble};
+use crate::double::Double;
 use crate::error::{Ior, KernelError};
 use crate::io::{Io, NoIo};
 use crate::log::debug;
@@ -8,9 +8,13 @@ use crate::parser;
 use crate::vm::{Op, Stop, Vm};
 use crate::{BL, Error, FALSE, Result, SIZE, TRUE};
 
+mod env;
 mod layout;
 
+use env::Environment;
 use layout::{INPUT_BUFFER_SIZE, Layout};
+
+pub use env::Config;
 
 /// The maximum word length in bytes.
 const MAX_WORD_LEN: usize = 31;
@@ -32,68 +36,6 @@ pub type Builtin<M, I> = fn(&mut Kernel<M, I>) -> Result<()>;
 enum Token {
     Lit(usize),
     Name(&'static [u8]),
-}
-
-#[derive(Debug, Clone, Copy)]
-struct Environment {
-    /// User configuration.
-    pub config: Config,
-    /// The maximum length of a counted string (bytes).
-    pub counted_string: usize,
-    /// The size of one address unit (bits).
-    pub address_unit_bits: usize,
-    /// Whether floored division is the default.
-    pub floored: bool,
-    /// The maximum value of a character (*char*).
-    pub max_char: usize,
-    /// The maximum value of a signed double.
-    pub max_d: SignedDouble,
-    /// The maximum value of a signed integer.
-    pub max_n: isize,
-    /// The maximum value of an unsigned integer.
-    pub max_u: usize,
-    /// The maximum value of an unsigned double.
-    pub max_ud: Double,
-}
-
-/// System environment configuration.
-#[derive(Debug, Clone, Copy)]
-pub struct Config {
-    /// The size of the pictured numeric output buffer (bytes).
-    pub hold: usize,
-    /// The size of the `pad` scratch area (bytes).
-    pub pad: usize,
-    /// The number of cells in the return stack.
-    pub return_stack_cells: usize,
-    /// The number of cells in the data stack.
-    pub stack_cells: usize,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            hold: 64,
-            pad: 84,
-            return_stack_cells: 64,
-            stack_cells: 64,
-        }
-    }
-}
-
-impl Default for Environment {
-    fn default() -> Self {
-        Self {
-            config: Config::default(),
-            counted_string: u8::MAX as usize,
-            address_unit_bits: u8::BITS as usize,
-            floored: false,
-            max_char: u8::MAX as usize,
-            max_d: SignedDouble::MAX,
-            max_n: isize::MAX,
-            max_u: usize::MAX,
-            max_ud: Double::MAX,
-        }
-    }
 }
 
 /// The outer interpreter.
---
+++ src/kernel/env.rs
@@ -0,0 +1,63 @@
+use crate::double::{Double, SignedDouble};
+
+#[derive(Debug, Clone, Copy)]
+pub(super) struct Environment {
+    /// User configuration.
+    pub config: Config,
+    /// The maximum length of a counted string (bytes).
+    pub counted_string: usize,
+    /// The size of one address unit (bits).
+    pub address_unit_bits: usize,
+    /// Whether floored division is the default.
+    pub floored: bool,
+    /// The maximum value of a character (*char*).
+    pub max_char: usize,
+    /// The maximum value of a signed double.
+    pub max_d: SignedDouble,
+    /// The maximum value of a signed integer.
+    pub max_n: isize,
+    /// The maximum value of an unsigned integer.
+    pub max_u: usize,
+    /// The maximum value of an unsigned double.
+    pub max_ud: Double,
+}
+
+impl Default for Environment {
+    fn default() -> Self {
+        Self {
+            config: Config::default(),
+            counted_string: u8::MAX as usize,
+            address_unit_bits: u8::BITS as usize,
+            floored: false,
+            max_char: u8::MAX as usize,
+            max_d: SignedDouble::MAX,
+            max_n: isize::MAX,
+            max_u: usize::MAX,
+            max_ud: Double::MAX,
+        }
+    }
+}
+
+/// System environment configuration.
+#[derive(Debug, Clone, Copy)]
+pub struct Config {
+    /// The size of the pictured numeric output buffer (bytes).
+    pub hold: usize,
+    /// The size of the `pad` scratch area (bytes).
+    pub pad: usize,
+    /// The number of cells in the return stack.
+    pub return_stack_cells: usize,
+    /// The number of cells in the data stack.
+    pub stack_cells: usize,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            hold: 64,
+            pad: 84,
+            return_stack_cells: 64,
+            stack_cells: 64,
+        }
+    }
+}
